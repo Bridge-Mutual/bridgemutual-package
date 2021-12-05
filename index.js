f223bad99523de4ad1a15487915ca3d8e75a50cb
vladimir <91060273+vladimirIdeaSoft@users.noreply.github.com>
2021-12-05 22:43:00 +0200

    0.1.4v
    0.1.4v

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import {
   _getWhitelistedContracts,
   getApprove,
   policyPurchase,
+  resetAllowance,
+  getAllowance,
+  setAllowance,
   getCoverageApprove,
   provideCoverage,
   getPurchasedPolicies} from './methods'
 export {
   _getWhitelistedContracts,
+  resetAllowance,
   getApprove,
   policyPurchase,
+  getAllowance,
+  setAllowance,
   getCoverageApprove,
   provideCoverage,
   getPurchasedPolicies
--- methods.js
+++ methods.js
@@ -1,17 +1,13 @@
-import ContractRegistryAbi from './contartsABI/ContractRegistryAbi.json'
-import PolicyBookRegistryContractAbi from './contartsABI/PolicyBookRegistryContractAbi.json'
 import PolicyBookFascade from './contartsABI/PolicyBookFascade.json'
 import PolicyBook from './contartsABI/PolicyBook.json'
-import PolicyBookContractAbi from './contartsABI/PolicyBookContractAbi.json'
 import USDTContractAbi from './contartsABI/USDTContractAbi.json'
-const CONTRACT_REGISTRY_PROXY_ADDRESS_TEST = '0xD36973F7d97660fa69c8A3daCC037a7568a69EbB';
-const CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN = '0x88240185a74F020B94b14FAe3e6d5DdE1AA9057b';
-const CONTRACT_POLICY_BOOK_ADDRESS_TEST = '0xA494E39f08540c9db0f347DE9C85ff2eA71e09F7';
+import PolicyBookRegistry from './contartsABI/PolicyBookRegistry.json'
+import ContractRegistry from './contartsABI/ContractRegistry.json'
+
 import PolicyRegistryAbi from './contartsABI/PolicyRegistry.json'
 import BigNumber from 'bignumber.js';
-
-const BMIContractStakingMainNet = '0x6771Fd8968488Eb590Dff1730FE099c0eFA415bF';
-const BMIContractStakingTestNet = '0x3a9956d5a362ed9e59f064b8bf3cf20b51fa8be2';
+const CONTRACT_REGISTRY_PROXY_ADDRESS_TEST = '0x643e8CbB0756462B8D3031c2663010607f138fC2';
+const CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN = '0x88240185a74F020B94b14FAe3e6d5DdE1AA9057b';
 /**
  * async function
  * Get whitelisted policy list
@@ -33,6 +29,22 @@ export async function _getWhitelistedContracts(web3, isTest) {
 
 /**
  * async function
+ * Get instance of Policy Book Registry Contract
+ * @param {Object} web3 - instance of web3
+ * @param {Boolean} isTest - for use Rinkeby TestNet
+ * @returns {Object} List of whitelisted polices
+ */
+async function getPolicyBooks(web3, isTest) {
+    let contract = new web3.eth.Contract(
+        ContractRegistry,
+        isTest ? CONTRACT_REGISTRY_PROXY_ADDRESS_TEST : CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN
+    );
+    return new web3.eth.Contract(PolicyBookRegistry,
+        await contract.methods.getPolicyBookRegistryContract().call());
+}
+
+/**
+ * async function
  * Get Approve
  * @param {Number} id - Id of policy
  * @param {Object} web3 - instance of web3
@@ -44,11 +56,11 @@ export async function _getWhitelistedContracts(web3, isTest) {
 
 export async function getApprove(id, web3, weeks, amount, userAddress, isTest) {
     let contract = new web3.eth.Contract(
-        PolicyBookContractAbi,
+        PolicyBook,
         id
     );
     const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
-    return contract.methods.getPolicyPrice(weeks, bigNumberAmount).call().then(res => {
+    return contract.methods.getPolicyPrice(weeks, bigNumberAmount, userAddress).call().then(res => {
         return getUSDTContract(web3, isTest).then(usdtContract => {
             return usdtContract.methods.allowance(userAddress, id).call().then(allowance => {
                 if (BigNumber(allowance).lt(BigNumber(res.totalPrice).idiv(10^12))) {
@@ -83,12 +95,9 @@ export async function getApprove(id, web3, weeks, amount, userAddress, isTest) {
  * @returns policy purchase result
  */
 
-export async function policyPurchase (userAddress, weeks, amount, referralAddress = '0x0000000000000000000000000000000000000000') {
+export async function policyPurchase (contract, policyId, userAddress, weeks, amount, referralAddress = '0x0000000000000000000000000000000000000000') {
     const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
-    let policyBookInstance = new web3.eth.Contract(
-        PolicyBook, CONTRACT_POLICY_BOOK_ADDRESS_TEST
-    );
-    policyBookInstance.methods.policyBookFacade().call().then(facadeId => {
+    contract.methods.policyBookFacade().call().then(facadeId => {
         let policyBookFacadeInstace = new web3.eth.Contract(
             PolicyBookFascade, facadeId
         );
@@ -102,22 +111,6 @@ export async function policyPurchase (userAddress, weeks, amount, referralAddres
 
 /**
  * async function
- * Get instance of Policy Book Registry Contract
- * @param {Object} web3 - instance of web3
- * @param {Boolean} isTest - for use Rinkeby TestNet
- * @returns {Object} List of whitelisted polices
- */
-async function getPolicyBooks(web3, isTest) {
-    let contract = new web3.eth.Contract(
-        ContractRegistryAbi,
-        isTest ? CONTRACT_REGISTRY_PROXY_ADDRESS_TEST: CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN
-    );
-    return new web3.eth.Contract(PolicyBookRegistryContractAbi,
-        await contract.methods.getPolicyBookRegistryContract().call());
-}
-
-/**
- * async function
  * Get instance of USDT contract
  * @param {Object} web3 - instance of web3
  * @param {Boolean} isTest - for use Rinkeby TestNet
@@ -125,8 +118,8 @@ async function getPolicyBooks(web3, isTest) {
  */
 async function getUSDTContract(web3, isTest) {
     let contract = new web3.eth.Contract(
-        ContractRegistryAbi,
-        isTest ? CONTRACT_REGISTRY_PROXY_ADDRESS_TEST: CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN
+        ContractRegistry,
+        isTest ? CONTRACT_REGISTRY_PROXY_ADDRESS_TEST : CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN
     );
     return new web3.eth.Contract(USDTContractAbi,
         await contract.methods.getUSDTContract().call());
@@ -144,23 +137,17 @@ async function getUSDTContract(web3, isTest) {
  * @returns provide coverage result
  */
 export async function provideCoverage(contract, userAddress, amount) {
-    const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
-    return contract.methods.convertSTBLToBMIX(bigNumberAmount).call().then((BMIxAmount) => {
-        return contract.methods.allowance(userAddress, isTest ? BMIContractStakingTestNet : BMIContractStakingMainNet).call().then((allowance) => {
-            if (BigNumber(allowance).lt(BMIxAmount)) {
-                return contract.methods.approve(isTest ? BMIContractStakingTestNet : BMIContractStakingMainNet, BMIxAmount).send({from: userAddress}).then(() => {
-                    return contract.methods.addLiquidityAndStake(bigNumberAmount, bigNumberAmount).send({from: userAddress}).then(result => {
-                        return result
-                    });
-                })
-            } else {
-                return contract.methods.addLiquidityAndStake(bigNumberAmount, bigNumberAmount).send({from: userAddress}).then(result => {
-                    return result
-                });
-            }
-        })
+    contract.methods.policyBookFacade().call().then(facadeId => {
+        let policyBookFacadeInstace = new web3.eth.Contract(
+            PolicyBookFascade, facadeId
+        );
 
+        const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
+        return policyBookFacadeInstace.methods.addLiquidity(bigNumberAmount).send({from: userAddress}).then(result => {
+            return result
+        })
     })
+
 }
 
 /**
@@ -176,10 +163,9 @@ export async function provideCoverage(contract, userAddress, amount) {
 
 export async function getCoverageApprove (id, web3, amount, userAddress, isTest) {
     let contract = new web3.eth.Contract(
-        PolicyBookContractAbi,
+        PolicyBook,
         id
     );
-
     const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
     return getUSDTContract(web3, isTest).then(usdtContract => {
         return usdtContract.methods.allowance(userAddress, id).call().then(allowance => {
@@ -202,52 +188,39 @@ export async function getCoverageApprove (id, web3, amount, userAddress, isTest)
     })
 }
 
-/**
- * async function
- * Request to receive reward (you can receive reward after 8 days)
- * @param {Number} id - Id of contract
- * @param {Object} web3 - instance of web3
- * @param {Boolean} isTest - for use Rinkeby TestNet
- * @param {Number} userAddress - userAddress from web3
- * @returns void
- */
-export async function unStake(id, web3, userAddress, isTest) {
-    let BMIStakingContract = new web3.eth.Contract(
-        BMICoverStaking,
-        isTest ? BMIContractStakingTestNet : BMIContractStakingMainNet
-    );
+export async function getAllowance (web3, userAddress, id, isTest) {
+    return getUSDTContract(web3, isTest).then(usdtContract => {
+        return usdtContract.methods.allowance(userAddress, id).call().then(allowance => {
+            return allowance;
+        })
+    })
+}
 
-    let contract = new web3.eth.Contract(
-        PolicyBookContractAbi,
-        id
-    );
 
-    BMIStakingContract.methods.withdrawStakerFundsWithProfit(id).send({from: userAddress}).then(() => {
-        contract.methods.getAvailableBMIXWithdrawableAmount(userAddress).call().then((amount) => {
-            contract.methods.approve(id, amount).send({from: userAddress}).then(() => {
-                contract.methods.requestWithdrawal(amount).send({from: userAddress}).then()
+
+export async function resetAllowance (web3, userAddress, id, isTest) {
+    return getUSDTContract(web3, isTest).then(usdtContract => {
+        return usdtContract.methods.allowance(userAddress, id).call().then(allowance => {
+            return usdtContract.methods.approve(id, BigNumber(0).idiv(10^12).toFixed()).send({from: userAddress}).then((result) => {
+                return result
             })
         })
-
     })
-
 }
 
-/**
- * async function
- * Request to receive liquidity (you can receive reward after 8 days)
- * @param {Number} id - Id of contract
- * @param {Object} web3 - instance of web3
- * @param {Number} userAddress - userAddress from web3
- * @returns void
- */
-export async function withdrawLiquidity(id, web3, userAddress) {
-    let contract = new web3.eth.Contract(
-        PolicyBookContractAbi,
-        id
-    );
-    contract.methods.withdrawLiquidity().send({from: userAddress}).then();
+
+export async function setAllowance (web3, userAddress, id, amount, isTest) {
+    const bigNumberAmount = BigNumber(amount).times(BigNumber(10).pow(18)).toFixed();
+    return getUSDTContract(web3, isTest).then(usdtContract => {
+        return usdtContract.methods.allowance(userAddress, id).call().then(allowance => {
+            return usdtContract.methods.approve(id, BigNumber(bigNumberAmount).idiv(10^12).toFixed()).send({from: userAddress}).then((result) => {
+                return result
+            })
+        })
+    })
 }
+
+
 
 /**
  * Get purchased insurance
@@ -262,13 +235,13 @@ export async function withdrawLiquidity(id, web3, userAddress) {
 
 export async function getPurchasedPolicies(web3, userAddress, active, offset, limit, isTest) {
     let contract = new web3.eth.Contract(
-        ContractRegistryAbi,
+        ContractRegistry,
         isTest ? CONTRACT_REGISTRY_PROXY_ADDRESS_TEST: CONTRACT_REGISTRY_PROXY_ADDRESS_MAIN
     );
     return contract.methods.getPolicyRegistryContract().call().then(res => {
         if (res) {
             let registryContract = new web3.eth.Contract(PolicyRegistryAbi, res);
-            return registryContract.methods.getPoliciesInfo(userAddress, active, offset, limit).call({from: userAddress}).then( (info) => {
+            return registryContract.methods.getPoliciesInfo(userAddress, active, 0, limit).call({from: userAddress}).then( (info) => {
                 return info
             })
         }
